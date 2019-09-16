5f16b542993e830236a17cfa6bf2d8ff113f22ca
caufman <caufman@yandex.ru>
2019-09-16 19:12:38 +0300

    Remove bot_token from code
    - hardcode token for tgbot
    + read token from file `bot.token`

--- settings.py
+++ settings.py
@@ -3,4 +3,5 @@ from os import path as osp
 
 BASE_DIR = osp.abspath(osp.dirname(__file__))
 
-BOT_TOKEN = '856212558:AAHjTTJvmNATH8_oH-7kwUyOIZO9nDp3TEg'
+with open(osp.join(BASE_DIR, 'bot.token'), mode="r", encoding='utf-8') as bt_file:
+    BOT_TOKEN = bt_file.readline().strip()
