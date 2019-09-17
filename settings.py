723a4e24396c7b0d488db998062336557e26147b
caufman <caufman@yandex.ru>
2019-09-17 12:36:37 +0300

    Add BASE_POINT_URL in settings.py

--- settings.py
+++ settings.py
@@ -2,6 +2,8 @@ from os import path as osp
 
 
 BASE_DIR = osp.abspath(osp.dirname(__file__))
+BASE_POINT_URL = r'http://point.im/api/'
 
 with open(osp.join(BASE_DIR, 'bot.token'), mode="r", encoding='utf-8') as bt_file:
     BOT_TOKEN = bt_file.readline().strip()
+
