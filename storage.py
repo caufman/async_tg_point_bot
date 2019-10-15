92cd5d3458fb5d070d15704974f4d34af7475b66
caufman <caufman@yandex.ru>
2019-10-15 06:07:28 +0300

    Add connect to pgsql and create table

--- storage.py
+++ storage.py
@@ -1,7 +1,32 @@
 import settings
 
 import asyncio
-import aiopg
+import psycopg2 as pg
+
+connect_string = f'dbname={settings.DATABASE["database"]} user={settings.DATABASE["role"]} password={settings.DATABASE["password"]} host={settings.DATABASE["host"]} port={settings.DATABASE["port"]}'
+
+async def create_auth_table():
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS auth_user (
+            id SERIAL PRIMARY KEY,
+            tg_username VARCHAR(255) UNIQUE NOT NULL,
+            tg_userid VARCHAR(255) UNIQUE NOT NULL,
+            point_username VARCHAR(255),
+            API_token VARCHAR(255),
+            CSRF_token VARCHAR(255)
+        )
+                      ''')
+    print(f'Создана таблица "auth_user".')
 
 
-db = aiopg.
+async def run_func(func, args=None, kwargs=None):
+    loop.run_until_complete(func(*args, **kwargs))
+
+
+
+
+
+conn = pg.connect(connect_string)
+cur = conn.cursor()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(create_auth_table())
